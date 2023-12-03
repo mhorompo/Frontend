4b59c2cd277ff0bc4c053160162d1b635e674917
Hörömpő Márk <mhorompo@gmail.com>
2023-12-03 10:49:50 +0100

    regist proba

--- src/app/components/regist/regist.component.ts
+++ src/app/components/regist/regist.component.ts
@@ -3,14 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-regist',
   templateUrl: './regist.component.html',
-  styleUrls: ['./regist.component.css']
+  styleUrls: ['./regist.component.css'],
 })
 export class RegistComponent {
+  email: string = '';
+  firstname: string = '';
+  lastname: string = '';
+  password: string = '';
+  passwordagain: string = '';
 
-  email: string='';
-  firstname: string='';
-  lastname: string='';
-  password: string='';
-  passwordagain: string='';
-
+  regist() {
+    console.log('regist');
+  }
 }
